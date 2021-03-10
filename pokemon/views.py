e4420f2fab5c5f47fd67ebe372375da0f78d2281
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-10 20:10:43 -0300

    new class based view for searching

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,5 +1,17 @@
+import requests
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
     template_name = 'pokemon/home.html'
+
+
+class SearchResultsListView(TemplateView):
+    template_name = 'pokemon/search.html'
+
+    def get_context_data(self, **kwargs):
+        query = self.request.GET.get('q')
+        r =
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'veer'
+        return context
