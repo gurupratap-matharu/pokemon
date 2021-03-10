4f34cfd023c8597b98a250d179ff1eac1aee9c73
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-10 20:10:31 -0300

    added endpoint for search results

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from pokemon.views import HomePageView
-
-app_name = 'pokemon'
+from pokemon.views import HomePageView, SearchResultsListView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
