5cdd41c37b02ab4ba343c81d8c03e06cf96a60b9
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-13 14:40:51 -0300

    added endpoint for detail view for each pokemon

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from pokemon.views import HomePageView, SearchResultsListView
+from pokemon.views import (HomePageView, PokemonDetailView,
+                           SearchResultsListView)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('search/', SearchResultsListView.as_view(), name='search_results'),
+    path('p/<str:name>/', PokemonDetailView.as_view(), name='pokemon_detail'),
+
 ]
